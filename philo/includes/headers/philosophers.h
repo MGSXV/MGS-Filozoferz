cd8148a057525718dac0b47c7b1cdd23a7516461
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-05-23 19:42:06 +0100

    Adding some functions for parsing

--- philo/includes/headers/philosophers.h
+++ philo/includes/headers/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:26:47 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/05/21 15:55:22 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/05/21 16:07:56 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 // Helpers functions
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_isdigit(int c);
 
 // Managing inputs functions
 bool	manage_inputs(int c, char **inputs);
--- philo/src/app/manage_inputs.c
+++ philo/src/app/manage_inputs.c
@@ -6,21 +6,37 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 15:52:03 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/05/21 16:03:13 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/05/21 16:35:03 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/philosophers.h"
 
+bool	ft_isnumber(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
 bool	manage_inputs(int c, char **inputs)
 {
+	int	i;
+
+	i = 0;
 	if (c == 2 && !ft_strncmp(inputs[1], "--help", ft_strlen("--help")))
 	{
-		ft_printf("\"philosophers\" requires 4 to 5 passed as fllows:\n\
+		ft_printf("\"philosophers\" requires 4 to 5 arguments passed as fllows:\n\
 $> ./philo arg1 arg2 arg3 arg4 arg5 ...\n");
 		return (false);
 	}
-	if (c != 5 && c !=6)
+	if (c != 5 && c != 6)
 	{
 		ft_printf("Invalid number of arguments!\n\
 Try \"--help\" for more information.\n");
@@ -28,8 +44,15 @@ Try \"--help\" for more information.\n");
 	}
 	else
 	{
-		
+		while (++i < c)
+		{
+			if (!ft_isnumber(inputs[i]))
+			{
+				ft_printf("Arguments error!\nTry \"--help\"\n\
+for more information.\n");
+				return (false);
+			}
+		}
 	}
 	return (true);
 }
-
