ab6eb3349825e013d094978f1972e4fcb8026771
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-05-24 17:43:05 +0100

    Editing header files

--- philo/includes/headers/philosophers.h
+++ philo/includes/headers/philosophers.h
@@ -6,19 +6,28 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:26:47 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/05/23 19:47:52 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/05/24 17:40:27 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdbool.h>
+#ifndef PHILOSOPHERS_H
 
-#include "types.h"
-#include "../ft_printf/ft_printf.h"
+# define PHILOSOPHERS_H
+# include<stdio.h>
+# include<stdbool.h>
+
+# include "types.h"
+# include "../ft_printf/ft_printf.h"
 
 // Helpers functions
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_isdigit(int c);
+int	ft_atoi(const char *str);
 
-// Managing inputs functions
+// App functions
 bool	manage_inputs(int c, char **inputs);
+
+// Init functions
+void	init_app(t_philosophers *ph, char **v, char c);
+
+#endif
--- philo/includes/headers/types.h
+++ philo/includes/headers/types.h
@@ -6,9 +6,13 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 19:45:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/05/23 19:47:39 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/05/24 17:40:58 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef TYPES_H
+
+# define TYPES_H
 
 typedef struct s_philosophers
 {
@@ -18,3 +22,5 @@ typedef struct s_philosophers
 	int	time_to_sleep;
 	int	eat_num;
 }	t_philosophers;
+
+#endif
