368166fe593ab28f1c2bd53a58bb6d7d0aa91543
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-05-24 17:42:37 +0100

    Init philosophers

---
+++ philo/src/init/init_app.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_app.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 20:00:57 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/05/24 17:37:44 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/headers/philosophers.h"
+
+void	init_app(t_philosophers *ph, char **v, char c)
+{
+	if (c == 5)
+	{
+		ph->philos_num = ft_atoi(v[1]);
+		ph->time_to_die = ft_atoi(v[2]);
+		ph->time_to_eat = ft_atoi(v[3]);
+		ph->time_to_sleep = ft_atoi(v[4]);
+	}
+	if (c == 6)
+		ph->eat_num = ft_atoi(v[5]);
+}
