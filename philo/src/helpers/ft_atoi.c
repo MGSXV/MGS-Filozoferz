447696c65615041da8afa456515f995cbe50eda8
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-05-24 17:42:48 +0100

    Add ft_atoi function

---
+++ philo/src/helpers/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 17:32:51 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/05/24 17:33:28 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/headers/philosophers.h"
+
+static int	ft_isspace(int c)
+{
+	int	res;
+
+	res = 0;
+	if (c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v' || c == ' ')
+		res = 1;
+	return (res);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	i;
+	int	sign;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		result = result * 10 + str[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
