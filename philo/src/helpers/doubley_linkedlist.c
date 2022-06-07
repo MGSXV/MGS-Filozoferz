f602f822248395b04a4695ffc2d8b2a0dd33ebe2
Soufiane El-Khamlichi <05soufiane@gmail.com>
2022-06-07 11:25:19 +0100

    ft_new_node function

---
+++ philo/src/helpers/doubley_linkedlist.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   doubley_linkedlist.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 11:10:27 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/06/07 11:24:58 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/headers/philosophers.h"
+
+t_philosofer	*ft_new_node(t_table **table)
+{
+	t_philosofer	*tmp_philo;
+
+	tmp_philo = malloc(sizeof(t_philosofer));
+	if (!tmp_philo)
+		return (NULL);
+	tmp_philo->next_philo = NULL;
+	tmp_philo->next_philo = NULL;
+	tmp_philo->philo_id = -1;
+	return (tmp_philo);
+}
