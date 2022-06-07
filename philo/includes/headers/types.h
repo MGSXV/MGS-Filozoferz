9d1cf1cd6ae2b58f3b979b10acb263e8e8df55f6
Soufiane El-Khamlichi <05soufiane@gmail.com>
2022-06-07 11:06:10 +0100

    Rename data struct from t_info to t_table

--- philo/includes/headers/types.h
+++ philo/includes/headers/types.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 19:45:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/07 10:57:44 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/07 11:02:02 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 # define TYPES_H
 
-typedef struct s_philosophers
-{
-	int	philos_num;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	eat_num;
-}	t_philo_info;
-
 typedef struct s_philosopher
 {
 	int						philo_id;
 	struct s_philosopher	*next_philo;
 	struct s_philosopher	*prev_philo;
-	t_philo_info			*info;
 }	t_philosofer;
+
+typedef struct s_table
+{
+	int				philos_num;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eat_num;
+	t_philosofer	*head;
+}	t_table;
 
 #endif
