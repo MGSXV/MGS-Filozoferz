d470aaf4f697e28dff5815dd0370cbac307b8662
Soufiane El-Khamlichi <05soufiane@gmail.com>
2022-06-07 11:25:31 +0100

    Starting with main

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:24:18 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/07 10:57:55 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/07 11:18:14 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 int	main(int c, char **v)
 {
+	t_table	*table;
+
 	if (!manage_inputs(c, v))
 		return (0);
+	table = malloc(sizeof(t_table));
+	if (!table)
+		return (0);
+	init_app(&table, c, v);
 	return (0);
 }
