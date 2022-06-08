496ca87f24da27e2bb5200be4479e0326128b6c0
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-08 03:33:48 +0100

    init_philosophers function

--- philo/src/init/init_app.c
+++ philo/src/init/init_app.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 20:00:57 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/07 11:32:09 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/08 03:32:57 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,13 @@ void	init_app(t_table **table, int c, char **v)
 	if (c == 6)
 		(*table)->eat_num = ft_atoi(v[5]);
 	(*table)->head = NULL;
+}
+
+void	init_philosophers(t_table *table)
+{
+	int	i;
+
+	i = -1;
+	while (++i < table->philos_num)
+		ft_add_node(&table, ft_new_node(i + 1));
 }
