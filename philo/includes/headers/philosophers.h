cb784c7818290e95f79f20ed6110015c8b7a34e1
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-08 03:34:28 +0100

    Add functions to header file

--- philo/includes/headers/philosophers.h
+++ philo/includes/headers/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:26:47 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/07 11:30:26 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/08 03:32:40 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,18 @@
 # include "types.h"
 
 // Helpers functions
-int		ft_strncmp(char *s1, char *s2, size_t n);
-int		ft_isdigit(int c);
-int		ft_strlen(char *s);
-int		ft_atoi(const char *str);
-bool	ft_isnumber(char *str);
+int				ft_strncmp(char *s1, char *s2, size_t n);
+int				ft_isdigit(int c);
+int				ft_strlen(char *s);
+int				ft_atoi(const char *str);
+bool			ft_isnumber(char *str);
+t_philosofer	*ft_new_node(int id);
+void			ft_add_node(t_table **table, t_philosofer *node);
 
 // App functions
 bool	manage_inputs(int c, char **inputs);
-
 // Init functions
 void	init_app(t_table **table, int c, char **v);
+void	init_philosophers(t_table *table);
 
 #endif
