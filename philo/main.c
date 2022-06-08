f796a5e5b18eed318ec5ce13e73e7a50aea2441c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-08 03:35:16 +0100

    Correcting a double free bug

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:24:18 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/07 11:26:44 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/08 03:33:20 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int	main(int c, char **v)
 
 	if (!manage_inputs(c, v))
 		return (0);
-	table = malloc(sizeof(t_table *));
+	table = malloc(sizeof(t_table));
 	if (!table)
 		return (0);
 	init_app(&table, c, v);
+	init_philosophers(table);
 	return (0);
 }
