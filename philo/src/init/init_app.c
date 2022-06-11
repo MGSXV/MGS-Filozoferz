b99fbf4a1026c2dce09f7c562733e367896611ce
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-11 03:47:01 +0100

    Fixing a bug

--- philo/src/init/init_app.c
+++ philo/src/init/init_app.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 20:00:57 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/08 03:32:57 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/10 22:29:16 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 void	init_app(t_table **table, int c, char **v)
 {
-	if (c == 5)
-	{
-		(*table)->philos_num = ft_atoi(v[1]);
-		(*table)->time_to_die = ft_atoi(v[2]);
-		(*table)->time_to_eat = ft_atoi(v[3]);
-		(*table)->time_to_sleep = ft_atoi(v[4]);
-	}
+	(*table)->philos_num = ft_atoi(v[1]);
+	(*table)->time_to_die = ft_atoi(v[2]);
+	(*table)->time_to_eat = ft_atoi(v[3]);
+	(*table)->time_to_sleep = ft_atoi(v[4]);
 	if (c == 6)
 		(*table)->eat_num = ft_atoi(v[5]);
 	(*table)->head = NULL;
