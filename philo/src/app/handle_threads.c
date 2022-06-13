baf6a62981ba9cfa35e2e310cf8e1de62baa2e41
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-13 22:47:34 +0100

    Add create_threads function

--- philo/src/app/handle_threads.c
+++ philo/src/app/handle_threads.c
@@ -1,16 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   join_threads.c                                     :+:      :+:    :+:   */
+/*   handle_threads.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 21:36:59 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/13 21:45:35 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/13 22:41:26 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/philosophers.h"
+
+void	create_threads(t_table *table)
+{
+	int				i;
+	t_philosofer	*tmp;
+
+	i = -1;
+	tmp = table->head;
+	while (++i < table->philos_num)
+	{
+		pthread_create(&(tmp->philosopher), NULL, philosophers_handler, (void *) table);
+		tmp = tmp->next_philo;
+	}
+}
 
 void	join_threads(t_table *table)
 {
