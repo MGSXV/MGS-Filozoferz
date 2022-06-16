4fd7df9725a8c485f932becac5ce852c77b6eca6
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-16 22:00:54 +0100

    Additional error checks

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:24:18 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/14 00:52:42 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/16 21:59:29 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,12 @@ int	main(int c, char **v)
 	if (!table)
 		return (2);
 	init_app(&table, c, v);
-	init_philosophers(table);
-	create_threads(table);
-	join_threads(table);
+	if (!init_philosophers(table))
+		return (2);
+	if (!create_threads(table))
+		return (2);
+	if (!join_threads(table))
+		return (2);
 	free_all(&table);
 	return (0);
 }
