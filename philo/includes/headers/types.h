9d31ba473faf06857ebeeaa6ead150da7e5071f6
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-17 00:30:38 +0100

    Add two elements to t_philosofer

--- philo/includes/headers/types.h
+++ philo/includes/headers/types.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 19:45:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/13 21:14:27 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/06/16 23:18:45 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ typedef struct s_philosopher
 	struct s_philosopher	*next_philo;
 	struct s_philosopher	*prev_philo;
 	bool					is_dead;
+	long					last_meal;
+	int						num_times_eat;
 	void					*table;
 }	t_philosofer;
 
@@ -34,6 +36,7 @@ typedef struct s_table
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				eat_num;
+	long			t0;
 	t_philosofer	*head;
 }	t_table;
 
