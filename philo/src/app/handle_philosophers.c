cba5c64a87f89e594f95160c05df3a4affbbb770
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-17 18:44:18 +0100

    Replace table with ph->table

--- philo/src/app/handle_philosophers.c
+++ philo/src/app/handle_philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mgs <mgs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 22:38:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/17 18:39:35 by mgs              ###   ########.fr       */
+/*   Updated: 2022/06/17 18:44:03 by mgs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,25 @@
 void	*philosophers_handler(void *philo)
 {
 	t_philosofer	*ph;
-	t_table			*table;
 
 	ph = (t_philosofer *) philo;
-	table = (ph->table);
 	while (true)
 	{
 		pthread_mutex_lock(&(ph->fork));
-		behaviour("has taken a fork", time_now(table), ph->philo_id);
+		behaviour("has taken a fork", time_now(ph->table), ph->philo_id);
 		pthread_mutex_lock(&(ph->next_philo->fork));
-		behaviour("has taken a fork", time_now(table), ph->philo_id);
-		eating(table, ph);
+		behaviour("has taken a fork", time_now(ph->table), ph->philo_id);
+		eating(ph->table, ph);
 		pthread_mutex_unlock(&(ph->fork));
 		pthread_mutex_unlock(&(ph->next_philo->fork));
-		if (table->eat_num != -1 && ph->num_times_eat == table->eat_num)
+		if (ph->table->eat_num != -1 && ph->num_times_eat == ph->table->eat_num)
 			break ;
-		sleeping(table, ph);
-		thinking(table, ph);
-		if (table->time_to_die < timestamp_in_ms() - ph->last_meal)
+		sleeping(ph->table, ph);
+		thinking(ph->table, ph);
+		if (ph->table->time_to_die < timestamp_in_ms() - ph->last_meal)
 		{
-			behaviour("died", time_now(table), ph->philo_id);
-			return (table);
+			behaviour("died", time_now(ph->table), ph->philo_id);
+			return (ph->table);
 		}
 	}
 	return (NULL);
