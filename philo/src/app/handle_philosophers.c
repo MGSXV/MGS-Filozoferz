1daf941b3dd868da03a6972fda17cec7c2b54416
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-18 04:59:40 +0100

    Resolving deadlock (Maybe)

--- philo/src/app/handle_philosophers.c
+++ philo/src/app/handle_philosophers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_philosophers.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgs <mgs@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 22:38:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/17 20:22:44 by mgs              ###   ########.fr       */
+/*   Updated: 2022/06/18 04:56:48 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,29 @@ void	*philosophers_handler(void *philo)
 	ph = (t_philosofer *) philo;
 	while (!(ph->table->someonedied))
 	{
-		eating(ph->table, ph);
-		if (ph->table->eat_num != -1 && ph->num_times_eat == ph->table->eat_num)
-			break ;
-		sleeping(ph->table, ph);
+		if (ph->philo_id % 2)
+		{
+			eating(ph->table, ph);
+			if (ph->table->eat_num != -1 && ph->num_times_eat == ph->table->eat_num)
+				return (NULL);
+		}
+		else
+			sleeping(ph->table, ph);
+		if (!(ph->philo_id % 2))
+		{
+			eating(ph->table, ph);
+			if (ph->table->eat_num != -1 && ph->num_times_eat == ph->table->eat_num)
+				return (NULL);
+		}
+		else
+			sleeping(ph->table, ph);
 		thinking(ph->table, ph);
 		if (ph->table->time_to_die < timestamp_in_ms() - ph->last_meal)
 		{
 			ph->table->someonedied = true;
 			behaviour("died", time_now(ph->table), ph->philo_id);
-			return (ph->table);
+			return (NULL);
 		}
 	}
-	return (NULL);
+	return (ph->table);
 }
