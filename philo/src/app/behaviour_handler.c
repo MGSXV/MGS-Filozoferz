c260210b4a6a4172c97f60ee5e90a4fff019992d
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-19 17:24:00 +0100

    Fixing behaviour

--- philo/src/app/behaviour_handler.c
+++ philo/src/app/behaviour_handler.c
@@ -6,7 +6,7 @@
 /*   By: mgs <mgs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 22:31:29 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/19 17:12:33 by mgs              ###   ########.fr       */
+/*   Updated: 2022/06/19 17:22:05 by mgs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void	behaviour(long time, int philo_id, char *action)
 
 void	eating(t_table *table, t_philosofer *ph)
 {
-	pthread_mutex_lock(&(ph->fork));
-	behaviour("has taken a fork", time_now(ph->table), ph->philo_id);
-	pthread_mutex_lock(&(ph->next_philo->fork));
-	behaviour("has taken a fork", time_now(ph->table), ph->philo_id);
-	behaviour("is eating", time_now(table), ph->philo_id);
+	pthread_mutex_lock(&(table->forks[ph->philo_id - 1]));
+	pthread_mutex_lock(&(table->forks[ph->philo_id - 1]));
+	behaviour(time_now(ph->table), ph->philo_id, "has taken a fork");
+	behaviour(time_now(ph->table), ph->philo_id, "has taken a fork");
+	behaviour(time_now(table), ph->philo_id, "is eating");
 	usleep(table->time_to_eat * 1000);
-	pthread_mutex_unlock(&(ph->fork));
-	pthread_mutex_unlock(&(ph->next_philo->fork));
+	pthread_mutex_unlock(&(table->forks[ph->philo_id - 1]));
+	pthread_mutex_unlock(&(table->forks[ph->philo_id - 1]));
 	ph->last_meal = timestamp_in_ms();
 	ph->num_times_eat++;
 }
--- philo/src/app/handle_philosophers.c
+++ philo/src/app/handle_philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mgs <mgs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 22:38:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/19 17:08:46 by mgs              ###   ########.fr       */
+/*   Updated: 2022/06/19 17:18:10 by mgs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*philosophers_handler(void *philo)
 		if (ph->table->time_to_die < timestamp_in_ms() - ph->last_meal)
 		{
 			ph->table->someonedied = true;
-			behaviour("died", time_now(ph->table), ph->philo_id);
+			behaviour( time_now(ph->table), ph->philo_id, "died");
 			return (NULL);
 		}
 	}
