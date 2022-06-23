f7dec340e1d5ea68af6e90265b09b3092e8068c9
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-06-23 17:38:23 +0100

    d

--- philo_bonus/includes/headers/types.h
+++ philo_bonus/includes/headers/types.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   types.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgs <mgs@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 00:04:44 by mgs               #+#    #+#             */
-/*   Updated: 2022/06/23 00:04:56 by mgs              ###   ########.fr       */
+/*   Updated: 2022/06/23 16:39:04 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,23 @@
 
 # define TYPES_H
 
-# include "philosophers.h"
+# include "philosophers_bonus.h"
 
-typedef struct s_table	t_table;
-typedef struct s_philosopher
-{
-	int						philo_id;
-	pthread_t				philosopher;
-	struct s_philosopher	*next_philo;
-	struct s_philosopher	*prev_philo;
-	long					last_meal;
-	int						num_times_eat;
-	t_table					*table;
-}	t_philosofer;
+// typedef struct s_table	t_table;
+// typedef struct s_philosopher
+// {
+// 	int						philo_id;
+// 	pthread_t				philosopher;
+// 	struct s_philosopher	*next_philo;
+// 	struct s_philosopher	*prev_philo;
+// 	long					last_meal;
+// 	int						num_times_eat;
+// 	t_table					*table;
+// }	t_philosofer;
 
 typedef struct s_table
 {
+	int 			philo_id;
 	int				philos_num;
 	int				time_to_die;
 	int				time_to_eat;
@@ -38,9 +39,7 @@ typedef struct s_table
 	long			t0;
 	bool			someonedied;
 	int				how_many_eats;
-	pthread_mutex_t	print_locker;
-	pthread_mutex_t	*forks;
-	t_philosofer	*head;
+	// t_philosofer	*head;
 }	t_table;
 
 #endif
